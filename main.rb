8786f58cf710477d2702dfa90e2a7ea22935267e
penach0 <josemanuel.penacho@gmail.com>
2022-10-02 15:27:56 +0100

    Create user prompts
    - #ask_position method prompts user for a start and finish position

--- main.rb
+++ main.rb
@@ -6,8 +6,22 @@ board = Board.new.board
 def knight_moves(start, finish)
   path = Knight.new(start).find_path(finish)
 
-  puts "You made it in #{path.size - 1} moves! Here is your path:"
+  if path.size - 1 == 1
+    puts 'You made it in 1 move! Here is your path:'
+  else
+    puts "You made it in #{path.size - 1} moves! Here is your path:"
+  end
   path.each { |square| puts square.to_s }
 end
 
-knight_moves(['h', 8], ['a', 1])
+def ask_position(position)
+  print "Pick a #{position} position (format eg. 'd4' or 'e6'): "
+  loop do
+    ans = gets.chomp
+    return [ans[0], ans[1].to_i] if ans[0].between?('a', 'h') && ans[1].to_i.between?(1, 8)
+
+    print 'Not valid. Try again: '
+  end
+end
+
+knight_moves(ask_position('starting'), ask_position('finish'))
